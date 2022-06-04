6cc8b45760db13e414afdf79e556b913014b471a
kulak91 <kulakovdmitr@gmail.com>
2022-06-05 01:04:12 +0300

    played with colors

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let avatar = document.getElementById('avatar');
 function changeImage() {
     console.log("clicked", avatar.src)
+    let element = document.body;
+    let main = document.body.querySelector("main");
+    let resume = document.querySelector(".resume");
+    resume.classList.toggle("white-mode")
+    element.classList.toggle("pink-mode");
+
+
+
+
     avatar.src.includes('avatar') ?  avatar.src = avatar.src.replace("avatar", "kitty") : avatar.src = avatar.src.replace("kitty", "avatar")   
     
 }
