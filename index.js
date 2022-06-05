b46c54aef25ddfd25324c31b2468cae31673ef1a
kulak91 <kulakovdmitr@gmail.com>
2022-06-05 03:56:02 +0300

    Before sleep commit. Prepared for adding timeline

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Changing palettes section
 let avatar = document.getElementById('avatar');
 let introText = document.querySelector('.intro_text')
+let element = document.body;
+let resume = document.querySelector(".resume");
 let funnyIntro = "Meow. I am a Binary Studio cat. Still got 7 lives. Work in IT, Full Stack Evil, all I do is sleep and try to help out hoomans with their projects whenever they need me."
 let normalIntro = "Hello. I am a self-taught developer. Still got a lot of topics to learn. Solving riddles is my passion and programming fulfills it. My dream is to work in IT." 
 
 function changeImage() {
-    let element = document.body;
-    let resume = document.querySelector(".resume");
     element.classList.toggle("pink-mode");
     resume.classList.toggle("white-mode")
 
@@ -30,3 +30,5 @@ function changeIntro(mode) {
     }
      
 }
+
+// Timeline 
